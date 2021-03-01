43be5602d883eb1300d4497412648f69fe2b9a2c
Juan de la Parra <whosju@gmail.com>
2021-03-01 07:43:09 -0600

    Initial Commit

---
+++ RetailStorePractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RetailStorePractice
+//
+//  Created by Juan carlos De la parra on 01/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
